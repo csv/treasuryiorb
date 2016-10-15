b9ffec582c6b86d4ebf2cdf1dd03e90954a774c9
Thomas Levine <_@thomaslevine.com>
2016-10-15 20:00:51 +0000

    use api.treasury.io
    https://github.com/csvsoundsystem/federal-treasury-api/issues/190

--- lib/treasuryio.rb
+++ lib/treasuryio.rb
@@ -5,7 +5,7 @@ require 'cgi'
 def treasuryio(sql)
   # Use like this:
   # treasuryio('SELECT * FROM t1 limit 3')
-  endpoint = 'https://premium.scraperwiki.com/cc7znvq/47d80ae900e04f2/sql/?q='
+  endpoint = 'https://api.treasury.io/cc7znvq/47d80ae900e04f2/sql/?q='
   body = open(endpoint + (CGI.escape sql)).read
   JSON.parse body
 end
